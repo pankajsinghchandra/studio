12154bac34481545b325cbc3f82363d7154e5019
pankaj kumar <pankaj802222@gmail.com>
2025-09-29 11:11:10 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  projectId: 'studio-7425086098-7b3fd',
+  appId: '1:436062723611:web:d7efdd3af206ac5acd22be',
+  apiKey: 'AIzaSyAU-JZplrrAi_or3SZMIZsyqcE4h4zkmIg',
+  authDomain: 'studio-7425086098-7b3fd.firebaseapp.com',
+  measurementId: '',
+  messagingSenderId: '436062723611',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };

f727f2c543e165fc744dc5bdfb1eddd048fceb2c
pankaj kumar <pankaj802222@gmail.com>
2025-12-27 05:49:30 +0000

    ek baat batao is app me sab thik hai na abhi kyunki abhi mai project set

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   projectId: 'studio-7425086098-7b3fd',
-  appId: '1:436062723611:web:d7efdd3af206ac5acd22be',
+  appId: '1:436062723611:web:85100a17b385b130cd22be',
   apiKey: 'AIzaSyAU-JZplrrAi_or3SZMIZsyqcE4h4zkmIg',
   authDomain: 'studio-7425086098-7b3fd.firebaseapp.com',
   measurementId: '',

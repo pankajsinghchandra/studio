188e75a766172c5761a771048a3771f282d35d8f
pankaj kumar <pankaj802222@gmail.com>
2025-12-27 16:04:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7425086098-7b3fd",
+  "appId": "1:436062723611:web:b8cfa218a3045dc1cd22be",
+  "apiKey": "AIzaSyAU-JZplrrAi_or3SZMIZsyqcE4h4zkmIg",
+  "authDomain": "studio-7425086098-7b3fd.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "436062723611"
+};

952ecc32049bf2abd8479afd8d711bf0ad31200c
pankaj kumar <pankaj802222@gmail.com>
2026-03-08 09:19:57 +0000

    achhcha app ke icon me thoda aur change karo like flouwers/ribbon se ghi

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,7 @@ export default function Home() {
             Apni class select karo.
             </h2>
             {classes.length > 0 ? (
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
               {classes.map((c) => (
                   <Card 
                       key={c.id} 

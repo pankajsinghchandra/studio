b5d8b78fa7e160511fba50c93b4c6418df8708cc
pankaj kumar <pankaj802222@gmail.com>
2026-09-04 04:54:13 +0000

    kya mai ise delete kar sakta hu kyunki mujhe lagta hai ki ye api key lea

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,4 @@
+
 export const firebaseConfig = {
   "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,9 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
-// Your web app's Firebase configuration using environment variables for security
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -14,7 +13,6 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const storage = getStorage(app);
